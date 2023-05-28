46576be7a14f76ad931ebfc4907e731d0b1fbb12
NicoMiguelez <nicomiguelez9@gmail.com>
2023-05-28 10:08:39 -0300

    Agregar ejercicio conversion de grados

--- conversionGrados.js
+++ conversionGrados.js
@@ -2,5 +2,6 @@ let fahrenheit = 50;
 let celsius = (fahrenheit - 32) * 5/9;
 console.log(celsius);
 
-celsius = (75 - 32) * 5/9;
+fahrenheit = 75;
+celsius = (fahrenheit - 32) * 5/9;
 console.log(celsius);
