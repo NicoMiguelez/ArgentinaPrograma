071040a795b02f28a14b5f892b58441eac798510
NicoMiguelez <nicomiguelez9@gmail.com>
2023-06-01 20:46:27 -0300

    agrego archivo conversion Grados

---
+++ conversionGrados.js
@@ -0,0 +1,11 @@
+let fahrenheit = 50;
+let celsius = (fahrenheit - 32) * 5/9;
+console.log(celsius);
+
+// La respuesta es 10
+
+fahrenheit = 75;
+celsius = (fahrenheit - 32) * 5/9;
+console.log(celsius);
+
+// La respuesta es 23.88
