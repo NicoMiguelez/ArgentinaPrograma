cae870a3d9d4a2067fe1962ed83523a1dfbbb422
NicoMiguelez <nicomiguelez9@gmail.com>
2023-06-01 20:13:38 -0300

    modifique archivo areaRectangulo.js

--- Practico0/areaRectangulo.js
+++ Practico0/areaRectangulo.js
@@ -3,7 +3,11 @@ let altura = 3;
 let area = base * altura;
 console.log(area);
 
+//La respuesta es 15
+
 base = 12.5;
 altura = 5.3;
 area = base * altura;
 console.log(area);
+
+// La respuesta es 66.25
